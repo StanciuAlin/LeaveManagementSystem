feeb5b373e12a1551ec6374a778ba07ac28ef27e
Stanciu Alin <stanciualin98@yahoo.com>
2025-02-26 14:55:34 +0200

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Alin Marian",
+                DateOfBirth = new DateTime(1998, 09, 06)
+            };
+            return View(data);
+        }
+    }
+}
